17c9fd6050a6d3046b933d90ed9ac1e3e2cccccb
Wiktoria van Harneveldt <62669899+wiktoriavh@users.noreply.github.com>
2026-03-05 21:14:02 +0100

    refactor: use grid to move content around (#6)
    * refactor: use grid to move content around
    
    * style: update Aside component layout and improve grid structure in index page

--- app/components/Aside.vue
+++ app/components/Aside.vue
@@ -1,11 +1,11 @@
 <template>
-  <aside :class="{ 'card': true, 'mobile': mobile }">
+  <aside class="card">
     <DirectorySpan>
       /README.md
     </DirectorySpan>
     <div class="me">
       <h2>Hi there, I'm Wiktoria</h2>
-      <img src="https://placecats.com/100/100" alt="Wiktoria" >
+      <img src="https://placecats.com/100/100" alt="Wiktoria">
     </div>
     <p>I'm a software engineer and recently (like 2025) I started to become more interested in owning my own content. I
       am collecting vinyls from music that I enjoy, started buying digital music, and want to buy physical movies and
@@ -22,46 +22,18 @@
   </aside>
 </template>
 
-<script setup lang="ts">
-defineProps<{
-  mobile?: boolean;
-}>();
-</script>
-
 <style scoped>
 aside {
   display: flex;
   flex-direction: column;
-  width: 300px;
   gap: 1rem;
   transform: rotate(-.5deg);
 }
 
-.mobile {
-  display: none;
-}
-
 @media (max-width: 768px) {
-  aside.mobile {
-    display: flex;
-    width: 100%;
-    margin-bottom: 2rem;
-    position: relative;
-  }
-
-  aside.mobile img {
-
-    top: 1rem;
-    right: 2rem;
-  }
-
-  aside.mobile .me {
+  aside .me {
     text-align: left;
     justify-content: space-between;
-  }
-
-  aside {
-    display: none;
   }
 }
 
